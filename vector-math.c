36dd90d3d1dab6fda6acf1c8a5e6604d0cded376
TrashRatInTrash <Rueslin.Rana@outlook.com>
2025-04-01 17:21:51 +0300

    using a struct implementation of matrix for data encapsulation

--- vector-math.c
+++ vector-math.c
@@ -5,29 +5,70 @@
 #define UPPER_BOUND 100
 #define LOWER_BOUND 1
 
-void add_Vector_Vector() {}
+typedef struct {
+  int rows, columns;
+  int **data;
+} Matrix;
 
 /*
   randomize matrix of size rows x columns
   values are randomized between values LOWER_BOUND and UPPER_BOUND
 */
-void randomize_Matrix(int rows, int columns, int matrix[rows][columns]) {
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      matrix[i][j] = rand() % (UPPER_BOUND - LOWER_BOUND + 1) + LOWER_BOUND;
+void randomize_Matrix(Matrix matrix) {
+  for (int i = 0; i < matrix.rows; i++) {
+    for (int j = 0; j < matrix.columns; j++) {
+      matrix.data[i][j] =
+          rand() % (UPPER_BOUND - LOWER_BOUND + 1) + LOWER_BOUND;
     }
   }
 }
 
-void print_Matrix(int rows, int columns, int matrix[rows][columns]) {
+void print_Matrix(Matrix matrix) {
   printf("--\n");
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      printf("%d, ", matrix[i][j]);
+  for (int i = 0; i < matrix.rows; i++) {
+    for (int j = 0; j < matrix.columns; j++) {
+      printf("%d, ", matrix.data[i][j]);
     }
     printf("\n");
   }
   printf("--\n");
+}
+
+/*
+  allocate a matrix of size rows x columns
+  returns a Struct Matrix
+*/
+Matrix matrix_Init(int rows, int columns) {
+  Matrix mat;
+  mat.rows = rows;
+  mat.columns = columns;
+
+  mat.data = (int **)malloc(rows * sizeof(int *));
+  if (!mat.data) {
+    fprintf(stderr, "matrix malloc failed\n");
+    exit(1);
+  }
+
+  for (int i = 0; i < rows; i++) {
+    mat.data[i] = (int *)malloc(columns * sizeof(int));
+    if (!mat.data[i]) {
+      fprintf(stderr, "matrix malloc failed\n");
+      exit(1);
+    }
+
+    for (int j = 0; j < columns; j++) {
+      mat.data[i][j] = 0;
+    }
+  }
+
+  return mat;
+}
+
+void matrix_Free(Matrix mat) {
+  for (int i = 0; i < mat.rows; i++) {
+    free(mat.data[i]);
+  }
+  free(mat.data);
 }
 
 int main() {
@@ -36,16 +77,14 @@ int main() {
 
   int m_rows = 5;
   int m_cols = 10;
-  int matrix[m_rows][m_cols];
+  Matrix mat1 = matrix_Init(m_rows, m_cols);
 
-  int v_cols = m_cols;
-  int vector[v_cols][1];
+  int v_rows = m_cols;
+  Matrix vec1 = matrix_Init(v_rows, 1);
 
-  randomize_Matrix(m_rows, m_cols, matrix);
-  print_Matrix(m_rows, m_cols, matrix);
+  randomize_Matrix(mat1);
+  randomize_Matrix(vec1);
 
-  printf("\n");
-
-  randomize_Matrix(v_cols, 1, vector);
-  print_Matrix(v_cols, 1, vector);
+  print_Matrix(mat1);
+  print_Matrix(vec1);
 }
